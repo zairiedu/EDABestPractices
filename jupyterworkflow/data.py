8dda2b1209c003d6d366a52aaa711f1acf4d57cb
Yasser Zairi <yzairi@bioscript.ca>
2021-07-21 00:11:09 -0300

    refactor data download

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -26,6 +26,12 @@ def get_fremont_data(filename='Fremont.csv',
     """
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
+        
     data.columns = ['Total','East','West']
     return data
